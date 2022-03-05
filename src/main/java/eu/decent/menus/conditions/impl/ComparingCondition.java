1a152a9284d3b08e064a7a80577eff7d6a9060f5
d0by <dobbyvoe@gmail.com>
2022-03-05 10:39:56 +0100

    Conditions

--- src/main/java/eu/decent/menus/conditions/impl/ComparingCondition.java
+++ src/main/java/eu/decent/menus/conditions/impl/ComparingCondition.java
@@ -1,27 +1,57 @@
 package eu.decent.menus.conditions.impl;
 
+import eu.decent.library.hooks.PAPI;
 import eu.decent.menus.conditions.Condition;
+import eu.decent.menus.conditions.ConditionType;
 import eu.decent.menus.player.PlayerProfile;
 import org.jetbrains.annotations.NotNull;
 
 public class ComparingCondition extends Condition {
 
+    private final ConditionType type;
     private final String compare;
     private final String input;
 
-    public ComparingCondition(@NotNull String compare, @NotNull String input) {
-        this(false, compare, input);
+    public ComparingCondition(@NotNull ConditionType type, @NotNull String compare, @NotNull String input) {
+        this(false, type, compare, input);
     }
 
-    public ComparingCondition(boolean inverted, @NotNull String compare, @NotNull String input) {
+    public ComparingCondition(boolean inverted, @NotNull ConditionType type, @NotNull String compare, @NotNull String input) {
         super(inverted);
+        this.type = type;
         this.compare = compare;
         this.input = input;
     }
 
     @Override
     public boolean check(@NotNull PlayerProfile profile) {
-
+        String compare = PAPI.setPlaceholders(profile.getPlayer(), this.compare);
+        String input = PAPI.setPlaceholders(profile.getPlayer(), this.input);
+        if (type.name().startsWith("STRING")) {
+            switch (type) {
+                case STRING_CONTAINS:
+                    return input.contains(compare);
+                case STRING_EQUAL:
+                    return input.equals(compare);
+                case STRING_EQUAL_IGNORECASE:
+                    return input.equalsIgnoreCase(compare);
+            }
+        } else {
+            int compareInteger = Integer.parseInt(compare);
+            int inputInteger = Integer.parseInt(input);
+            switch (type) {
+                case LESS:
+                    return inputInteger < compareInteger;
+                case LESS_EQUAL:
+                    return inputInteger <= compareInteger;
+                case EQUAL:
+                    return inputInteger == compareInteger;
+                case GREATER_EQUAL:
+                    return inputInteger >= compareInteger;
+                case GREATER:
+                    return inputInteger > compareInteger;
+            }
+        }
         return false;
     }
 
--- src/main/java/eu/decent/menus/conditions/impl/ExpCondition.java
+++ src/main/java/eu/decent/menus/conditions/impl/ExpCondition.java
@@ -1,4 +1,27 @@
 package eu.decent.menus.conditions.impl;
 
-public class ExpCondition {
+import eu.decent.menus.conditions.Condition;
+import eu.decent.menus.player.PlayerProfile;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpCondition extends Condition {
+
+    private final int minLevel;
+
+    public ExpCondition(int minLevel) {
+        this(false, minLevel);
+    }
+
+    public ExpCondition(boolean inverted, int minLevel) {
+        super(inverted);
+        this.minLevel = minLevel;
+    }
+
+    @Override
+    public boolean check(@NotNull PlayerProfile profile) {
+        Player player = profile.getPlayer();
+        return player.getLevel() >= minLevel;
+    }
+
 }
