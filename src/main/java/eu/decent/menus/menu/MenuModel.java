898cec6bbff8130f733ec20ad72f4c7504193c60
d0by <dobbyvoe@gmail.com>
2022-03-07 20:55:30 +0100

    Cleanup

--- src/main/java/eu/decent/menus/menu/MenuModel.java
+++ src/main/java/eu/decent/menus/menu/MenuModel.java
@@ -13,7 +13,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.configuration.ConfigurationSection;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.EnumMap;
