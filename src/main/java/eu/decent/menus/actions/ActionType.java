5b09322e7a559a8eaec56c866fdd33e6baab4158
d0by <dobbyvoe@gmail.com>
2022-03-07 17:51:41 +0100

    Remove old actions

--- src/main/java/eu/decent/menus/actions/ActionType.java
+++ src/main/java/eu/decent/menus/actions/ActionType.java
@@ -1,14 +1,6 @@
 package eu.decent.menus.actions;
 
-import eu.decent.library.hooks.PAPI;
-import eu.decent.library.utils.Common;
-import eu.decent.menus.DecentMenus;
-import eu.decent.menus.menu.Menu;
-import eu.decent.menus.player.PlayerProfile;
-import eu.decent.menus.utils.BungeeUtils;
 import lombok.Getter;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,138 +15,52 @@ public enum ActionType {
     /**
      * Send the given message to the player.
      */
-    MESSAGE() {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null) {
-                Player player = profile.getPlayer();
-                data = PAPI.setPlaceholders(player, data);
-                Common.tell(player, data);
-            }
-        }
-    },
+    MESSAGE("message"),
     /**
      * Broadcast the given message across the server.
      */
-    BROADCAST("broadcast_message") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null) {
-                Player player = profile.getPlayer();
-                data = PAPI.setPlaceholders(player, data);
-                Bukkit.broadcastMessage(Common.colorize(data));
-            }
-        }
-    },
+    BROADCAST("broadcast", "broadcast message"),
 
     // -- Command Actions
 
     /**
      * Execute the given command as the player.
      */
-    COMMAND("player") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null && !data.isEmpty()) {
-                Player player = profile.getPlayer();
-                Bukkit.dispatchCommand(profile.getPlayer(), PAPI.setPlaceholders(player, data));
-            }
-        }
-    },
+    COMMAND("command", "player"),
     /**
      * Execute the given command as console.
      */
-    CONSOLE() {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null && !data.isEmpty()) {
-                Player player = profile.getPlayer();
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PAPI.setPlaceholders(player, data));
-            }
-        }
-    },
+    CONSOLE("console"),
     /**
      * Send the given message into chat as the player.
      */
-    CHAT() {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null && !data.isEmpty()) {
-                Player player = profile.getPlayer();
-                player.chat(Common.colorize(PAPI.setPlaceholders(player, data)));
-            }
-        }
-    },
+    CHAT("chat"),
 
     // -- Server Actions
 
     /**
      * Send player to the given server.
      */
-    CONNECT("server") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null && !data.isEmpty()) {
-                BungeeUtils.connect(profile.getPlayer(), data);
-            }
-        }
-    },
+    CONNECT("connect", "server"),
 
     // -- Menu Actions
 
     /**
      * Open the given menu.
      */
-    OPEN_MENU("open", "menu") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            if (data != null && !data.isEmpty()) {
-                DecentMenus.getInstance().getMenuRegistry().openMenu(profile.getPlayer(), data);
-            }
-        }
-    },
+    OPEN_MENU("open menu", "open", "menu"),
     /**
      * Open the previous menu if possible.
      */
-    PREVIOUS_MENU("previous", "prev", "prev_menu") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            Menu menu = profile.getOpenMenu();
-            if (menu == null) {
-                return;
-            }
-            Menu previousMenu = menu.getPreviousMenu();
-            if (previousMenu == null) {
-                return;
-            }
-            menu.close();
-            previousMenu.open();
-        }
-    },
+    PREVIOUS_MENU("previous menu", "previous", "prev", "prev menu"),
     /**
      * Refresh the players current menu if possible.
      */
-    REFRESH_MENU("update_menu", "refresh", "update") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            Menu menu = profile.getOpenMenu();
-            if (menu != null) {
-                menu.open();
-            }
-        }
-    },
+    REFRESH_MENU("refresh menu", "update menu", "refresh", "update"),
     /**
      * Close the players current menu if possible.
      */
-    CLOSE_MENU("close") {
-        @Override
-        public void execute(@NotNull PlayerProfile profile, String data) {
-            Menu menu = profile.getOpenMenu();
-            if (menu != null) {
-                menu.close();
-            }
-        }
-    }
+    CLOSE_MENU("close menu", "close"),
     ;
 
     private final Set<String> aliases;
@@ -169,14 +75,6 @@ public enum ActionType {
     }
 
     /**
-     * Execute this {@link ActionType} for the given {@link PlayerProfile} with the given data.
-     *
-     * @param profile The profile.
-     * @param data The data.
-     */
-    public abstract void execute(@NotNull PlayerProfile profile, String data);
-
-    /**
      * Get an {@link ActionType} from string if possible.
      *
      * @param string The string.
@@ -185,8 +83,10 @@ public enum ActionType {
     @Nullable
     public static ActionType fromString(@NotNull String string) {
         for (ActionType actionType : values()) {
-            if (actionType.name().equalsIgnoreCase(string) || actionType.getAliases().contains(string.toLowerCase())) {
-                return actionType;
+            for (String alias : actionType.getAliases()) {
+                if (alias.trim().equalsIgnoreCase(string.trim())) {
+                    return actionType;
+                }
             }
         }
         return null;
