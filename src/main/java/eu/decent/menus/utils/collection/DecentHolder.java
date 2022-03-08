22a232923cd6d4d16cf32aebdaab7e36fa785681
d0by <dobbyvoe@gmail.com>
2022-03-08 17:31:42 +0100

    Javadoc DecentHolder

--- src/main/java/eu/decent/menus/utils/collection/DecentHolder.java
+++ src/main/java/eu/decent/menus/utils/collection/DecentHolder.java
@@ -1,27 +1,62 @@
 package eu.decent.menus.utils.collection;
 
+/**
+ * This class represents an object that holds a list of something.
+ *
+ * @param <T> Type of the held objects.
+ */
 public class DecentHolder<T> {
 
+    /**
+     * The list storing all held objects.
+     */
     private final DList<T> list;
 
+    /**
+     * Create a new {@link DecentHolder}.
+     */
     public DecentHolder() {
         this.list = new DList<>();
     }
 
+    /**
+     * Create a new {@link DecentHolder}.
+     *
+     * @param cap The maximum expected size of this holder.
+     */
     public DecentHolder(int cap) {
         this.list = new DList<>(cap);
     }
 
+    /**
+     * Add a new object on the last place in this holder.
+     *
+     * @param t The object.
+     * @return Instance of this.
+     */
     public DecentHolder<T> append(T t) {
         list.add(t);
         return this;
     }
 
+    /**
+     * Add a new object on the first place in this holder.
+     *
+     * @param t The object.
+     * @return Instance of this.
+     */
     public DecentHolder<T> prepend(T t) {
         list.addFirst(t);
         return this;
     }
 
+    /**
+     * Insert a new object on the specified index in this holder.
+     *
+     * @param index The index.
+     * @param t The object.
+     * @return Instance of this.
+     */
     public DecentHolder<T> insert(int index, T t) {
         if (inBounds(index)) {
             list.add(index, t);
@@ -29,13 +64,13 @@ public class DecentHolder<T> {
         return this;
     }
 
-    public DecentHolder<T> get(int index, T t) {
-        if (inBounds(index)) {
-            list.set(index, t);
-        }
-        return this;
-    }
-
+    /**
+     * Set a new object on the specified index in this holder.
+     *
+     * @param index The index.
+     * @param t The object.
+     * @return Instance of this.
+     */
     public DecentHolder<T> set(int index, T t) {
         if (inBounds(index)) {
             list.set(index, t);
@@ -43,6 +78,12 @@ public class DecentHolder<T> {
         return this;
     }
 
+    /**
+     * Remove the object at the specified index in this holder.
+     *
+     * @param index The index.
+     * @return The removed object.
+     */
     public T remove(int index) {
         if (inBounds(index)) {
             return list.remove(index);
@@ -50,6 +91,12 @@ public class DecentHolder<T> {
         return null;
     }
 
+    /**
+     * Get the object at the specified index in this holder.
+     *
+     * @param index The index.
+     * @return The object.
+     */
     public T get(int index) {
         if (inBounds(index)) {
             return list.get(index);
@@ -57,10 +104,21 @@ public class DecentHolder<T> {
         return null;
     }
 
+    /**
+     * Get this holder as a list.
+     *
+     * @return The list.
+     */
     public DList<T> asList() {
         return list;
     }
 
+    /**
+     * Check whether the given index is in bounds of this holder.
+     *
+     * @param index The index.
+     * @return The requested boolean.
+     */
     public boolean inBounds(int index) {
         return index >= 0 && index < list.size();
     }
