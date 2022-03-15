44e8651dcb46b7c4d32bddb972ec4c40d166d972
d0by <dobbyvoe@gmail.com>
2022-03-15 21:10:35 +0100

    CFG min & max check impl, cleanup

--- src/main/java/eu/decent/menus/utils/config/CFG.java
+++ src/main/java/eu/decent/menus/utils/config/CFG.java
@@ -2,6 +2,8 @@ package eu.decent.menus.utils.config;
 
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,7 +12,8 @@ import java.lang.reflect.Modifier;
 
 public final class CFG {
 
-    public static YamlConfiguration load(Object object, File file) {
+    @Nullable
+    public static YamlConfiguration load(@NotNull Object object, @NotNull File file) {
         try {
             file.getParentFile().mkdirs();
             YamlConfiguration config = saveIntoConfigurationFromObject(object);
@@ -27,18 +30,19 @@ public final class CFG {
         return null;
     }
 
-    public static void write(Object object, File file) throws Exception {
+    public static void write(@NotNull Object object, @NotNull File file) throws Exception {
         saveIntoConfigurationFromObject(object).save(file);
     }
 
-    public static void read(Object object, File file) throws IOException, InvalidConfigurationException {
+    public static void read(@NotNull Object object, @NotNull File file) throws IOException, InvalidConfigurationException {
         file.getParentFile().mkdirs();
         YamlConfiguration config = new YamlConfiguration();
         config.load(file);
         loadFromConfigurationToObject(object, config);
     }
 
-    public static YamlConfiguration saveIntoConfigurationFromObject(Object object) throws Exception {
+    @NotNull
+    public static YamlConfiguration saveIntoConfigurationFromObject(@NotNull Object object) throws Exception {
         YamlConfiguration config = new YamlConfiguration();
         boolean stat = object instanceof Class<?>;
         Class<?> clazz = stat ? (Class<?>) object : object.getClass();
@@ -56,29 +60,41 @@ public final class CFG {
         return config;
     }
 
-    public static void loadFromConfigurationToObject(Object object, YamlConfiguration config) {
+    public static void loadFromConfigurationToObject(@NotNull Object object, @NotNull YamlConfiguration config) {
         boolean stat = object instanceof Class<?>;
         Class<?> clazz = stat ? (Class<?>) object : object.getClass();
         for (Field f : clazz.getDeclaredFields()) {
-            f.setAccessible(true);
-            if (f.isAnnotationPresent(ConfigValue.class)) {
-                String key = f.getAnnotation(ConfigValue.class).value();
-                if (Modifier.isStatic(f.getModifiers())) {
-                    try {
-                        Object o = config.get(key);
-                        if (o != null) {
-                            f.set(null, o);
-                        }
-                    } catch (Throwable ignored) {}
-                } else if (!stat) {
-                    try {
-                        Object o = config.get(key);
-                        if (o != null) {
-                            f.set(object, o);
-                        }
-                    } catch (Throwable ignored) {}
-                }
+            if (!f.isAnnotationPresent(ConfigValue.class)) {
+                continue;
             }
+            f.setAccessible(true);
+
+            // -- Get the annotation
+            ConfigValue configValue = f.getAnnotation(ConfigValue.class);
+            String key = configValue.value();
+            double min = configValue.min();
+            double max = configValue.max();
+
+            try {
+                Object o = config.get(key);
+                if (o != null) {
+                    // -- If number, check min & max
+                    if (o instanceof Number) {
+                        Number number = (Number) o;
+                        double numberValue = number.doubleValue();
+                        if (numberValue < min || numberValue > max) {
+                            continue;
+                        }
+                    }
+
+                    // -- Set the field value
+                    if (Modifier.isStatic(f.getModifiers())) {
+                        f.set(null, o);
+                    } else if (!stat) {
+                        f.set(object, o);
+                    }
+                }
+            } catch (Throwable ignored) {}
         }
     }
 
