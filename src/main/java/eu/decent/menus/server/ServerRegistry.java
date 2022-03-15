7ea9a06854008b93a8b15a32b58eda5693cc03d3
d0by <dobbyvoe@gmail.com>
2022-03-15 20:04:24 +0100

    ServerRegistry reload impl

--- src/main/java/eu/decent/menus/server/ServerRegistry.java
+++ src/main/java/eu/decent/menus/server/ServerRegistry.java
@@ -1,7 +1,10 @@
 package eu.decent.menus.server;
 
+import eu.decent.menus.Config;
 import eu.decent.menus.utils.collection.Registry;
 import org.jetbrains.annotations.NotNull;
+
+import java.net.InetSocketAddress;
 
 /**
  * This class holds all pinged servers.
@@ -15,7 +18,23 @@ public class ServerRegistry extends Registry<String, Server> {
      */
     @Override
     public void reload() {
+        this.clear();
 
+        if (Config.PINGER_ENABLED) {
+            for (String serverString : Config.PINGER_SERVERS) {
+                if (!serverString.matches("\\S+:(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3}):\\d{1,5}")) {
+                    continue;
+                }
+                // -- Parse data
+                String[] spl = serverString.split(":");
+                String serverName = spl[0];
+                String serverAddress = spl[1];
+                int serverPort = Integer.parseInt(spl[2]);
+                // -- Register the server
+                Server server = new Server(serverName, new InetSocketAddress(serverAddress, serverPort));
+                this.register(server);
+            }
+        }
     }
 
     /**
